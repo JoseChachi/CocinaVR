d04eaf0f7747396e7d4af130b98be2b2a76ded68
JaimeRamosT <jaime.ramos@utec.edu.pe>
2024-06-24 20:51:42 -0500

    First ver menu

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        // Salir de la aplicaci�n
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
